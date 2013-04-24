ce66f03dd7341732964681437a431f9132448074
Ben Coverston <ben.coverston@gmail.com>
2013-04-24 15:47:58 -0600

    added todo to CF_Metadata and changed build identifier

--- src/java/org/apache/cassandra/config/CFMetaData.java
+++ src/java/org/apache/cassandra/config/CFMetaData.java
@@ -172,7 +172,9 @@ public final class CFMetaData
                                                      + "release_version text,"
                                                      + "rpc_address inet,"
                                                      + "data_center text,"
-                                                     + "rack text"
+                                                     + "rack text,"
+            //TODO: DSP-1968 Where workload is needed we should put an alter-table statement into our code
+                                                     + "workload text"
                                                      + ") WITH COMMENT='known peers in the cluster'");
 
     public static final CFMetaData PeerEventsCf = compile("CREATE TABLE " + SystemTable.PEER_EVENTS_CF + " ("
